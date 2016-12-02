a9534f6c2e32ee4fad900dcc1c4ed7c50ff786bf
Jeff Schreiner <jeff.s.schreiner@outlook.com>
2016-12-01 22:38:27 -0700

    Added Db Creation to the script

--- GloriousDb.sql
+++ GloriousDb.sql
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 -- Database: `GloriousDb`
 --
 
+CREATE DATABASE IF NOT EXISTS `GloriousDb`;
+USE `GloriousDb`;
 -- --------------------------------------------------------
 
 --
