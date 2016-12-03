de97f7da77f963d1beb2d4a40507cafd1f4f5be7
Jeff Schreiner <Jeff.S.Schreiner@gmail.com>
2016-12-02 22:13:56 -0700

    admin redirect

--- admin/index.php
+++ admin/index.php
@@ -1,11 +1,4 @@
 <?php
-// needed on all pages
-require_once('../config.php');
-include('server/view/header.php');
+// Redirect to the account edit screen.
+header("Location: account/index.php");
 ?>
-
-<h1>Template</h1>
-
-<p>Stuff here.</p>
-
-<?php include('server/view/footer.php'); ?>
